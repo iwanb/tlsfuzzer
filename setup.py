5b71315c9026dcaaf8d1b32ac2c7ad87def91e95
ibriquem <iwan.briquemont@nokia.com>
2024-11-19 13:56:13 +0100

    Add dependencies to setup and use setuptools to package it

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # Author: Hubert Kario
 # Released under Gnu GPL v2.0, see LICENSE file for details
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="tlsfuzzer",
       version="0.0.1",
@@ -12,4 +12,6 @@ setup(name="tlsfuzzer",
       url="https://github.com/tlsfuzzer/tlsfuzzer",
       description="TLS test suite and fuzzer.",
       license="GPLv2",
+      install_requires=["tlslite-ng==0.8.0-beta5"],
+      extras_require={"fastcrypto": ["m2crypto", "gmpy"]},
       packages=["tlsfuzzer", "tlsfuzzer.utils"])
